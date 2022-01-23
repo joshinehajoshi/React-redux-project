b1308e0223a97066e01b2d7a19d4fc141c1164b8
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-23 13:56:28 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Cart from './Components/Cart';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
     <Header />
-    <Home />
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
+
 function Home() {
     const data = () => {
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => 
-  console.log(json));
+        fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+        fetch("https://fakestoreapi.com/carts")
+        .then((res) => res.json())
+        .then((json) => console.log(json));
     }
   return ( 
         <div>
