fb99d014099c05ae613b6c5fadb054941d5fd095
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-23 14:07:07 +0530

    / path added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
